66e62dacf7bb94aea41e4216d4edd7a00c89f15f
d.nazarenko <d.nazarenko@eastbanctech.ru>
2020-03-30 23:05:06 +0300

    switched h2 to postgres for saving

--- ms-order-sql-saver/src/main/java/com/fooddelivery/order/persistence/OrderEntitySaver.java
+++ ms-order-sql-saver/src/main/java/com/fooddelivery/order/persistence/OrderEntitySaver.java
@@ -1,8 +1,8 @@
 package com.fooddelivery.order.persistence;
 
+import com.fooddelivery.order.event.BaseEvent;
 import com.fooddelivery.order.event.ConfirmOrderPaymentEvent;
 import com.fooddelivery.order.event.CreateOrderEvent;
-import com.fooddelivery.order.event.OrderEvent;
 import com.fooddelivery.order.model.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +17,7 @@ public class OrderEntitySaver {
     private final OrderRepository orderRepository;
 
     @KafkaListener(topics = "test", groupId = "group1")
-    public void on(OrderEvent event) { //todo switch to java 14 and try pattern matching
+    public void on(BaseEvent event) { //todo switch to java 14 and try pattern matching
         log.info("Event received: " + event.getClass().getName());
         if (event instanceof CreateOrderEvent) {
             on((CreateOrderEvent) event);
