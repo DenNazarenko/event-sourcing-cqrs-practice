689d5e8015095b24a4b43fc67f11bc8a50751789
d.nazarenko <d.nazarenko@eastbanctech.ru>
2020-05-04 15:21:44 +0300

    added saving events into hadoop

--- ms-event-saver-hdfs/src/main/java/com/fooddelivery/order/config/KafkaConfig.java
+++ ms-event-saver-hdfs/src/main/java/com/fooddelivery/order/config/KafkaConfig.java
@@ -2,9 +2,13 @@ package com.fooddelivery.order.config;
 
 import com.fooddelivery.order.event.BaseEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -32,5 +36,23 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, BaseEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, BaseEvent> eventsConsumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
+        return new DefaultKafkaConsumerFactory<>(
+                configProps,
+                new StringDeserializer(),
+                new JsonDeserializer<>(BaseEvent.class)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, BaseEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, BaseEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(eventsConsumerFactory());
+        return factory;
     }
 }
--- ms-event-saver-hdfs/src/main/java/com/fooddelivery/order/saver/EventSaver.java
+++ ms-event-saver-hdfs/src/main/java/com/fooddelivery/order/saver/EventSaver.java
@@ -1,10 +1,14 @@
 package com.fooddelivery.order.saver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fooddelivery.order.event.BaseEvent;
-import com.fooddelivery.order.event.ConfirmOrderPaymentEvent;
-import com.fooddelivery.order.event.CreateOrderEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +17,26 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class EventSaver {
 
-    @KafkaListener(topics = "order-events", groupId = "group1")
+    private final ObjectMapper objectMapper;
+
+    @SneakyThrows
+    @KafkaListener(topics = "order-events", groupId = "group1", containerFactory = "kafkaListenerContainerFactory")
     public void on(BaseEvent baseEvent) {
-        log.info("Event has been received");
-        if (baseEvent instanceof CreateOrderEvent) {
-            // todo save in hdfs
-        } else if (baseEvent instanceof ConfirmOrderPaymentEvent) {
-            // todo save in hdfs
+        log.info("Event [" + baseEvent.id + "] has been received. Starting saving to HDFS");
+
+        Configuration conf = new Configuration();
+        conf.set("dfs.replication", "1"); //todo could it be set into hadoop configs?
+        try (SequenceFile.Writer writer = SequenceFile.createWriter(
+                conf,
+                SequenceFile.Writer.file(new Path("hdfs://localhost:9000/events")), //todo think how to cut events to many files
+                SequenceFile.Writer.keyClass(Text.class),
+                SequenceFile.Writer.valueClass(Text.class),
+                SequenceFile.Writer.appendIfExists(true)
+        )) {
+            writer.append(new Text(baseEvent.id), new Text(objectMapper.writeValueAsString(baseEvent)));
         }
+
+        log.info("Event [" + baseEvent.id + "] has been saved");
     }
 
 }
